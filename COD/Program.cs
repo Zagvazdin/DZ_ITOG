25b3ea8cc04074a90583987ccf49ae4026bf2db3
Sergei <zagers72@bk.ru>
2023-07-01 23:29:13 +0500

    Код исправно работает

--- COD/Program.cs
+++ COD/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите исходный массив строк: ");// Введите  массив строк
+        string[] sourceArray = Console.ReadLine().Split(' ');
+    
+        string[] newArray = new string[sourceArray.Length];// Формируем новый массив из строк, длина которых меньше или равна 3 символам
+        int count = 0;
+        for (int i = 0; i < sourceArray.Length; i++)
+        {
+            if (sourceArray[i].Length <= 3)
+            {
+                newArray[count] = sourceArray[i];
+                count++;
+            }
+        }
+        
+        Console.WriteLine("Новый массив строк:");// Печатаем новый массив
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(newArray[i] + " ");
+        }
+    }
+}
+
